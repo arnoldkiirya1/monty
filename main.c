e578123468c67d82b472c6da1ec6b8901e9dba35
Kiirya Arnold <kiiryarnold@gmail.com>
2023-05-25 08:21:21 -0700

    updated

--- main.c
+++ main.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "monty.h"
 
 bus_t bus = {NULL, NULL, NULL, 0};
@@ -10,44 +12,41 @@ bus_t bus = {NULL, NULL, NULL, 0};
  */
 int main(int argc, char *argv[])
 {
-	FILE *file;
-	size_t size = 0;
-	ssize_t read_line = 1;
-	stack_t *stack = NULL;
-	unsigned int counter = 0;
+        FILE *file;
+        size_t size = 0;
+        ssize_t read_line;
+        stack_t *stack = NULL;
+        unsigned int counter = 0;
+        char *content = NULL;
+        const int MAX_LINE_LENGTH = 1024;
 
-	if (argc != 2)
-	{
-		fprintf(stderr, "USAGE: monty file\n");
-		exit(EXIT_FAILURE);
-	}
+        if (argc != 2)
+        {
+                fprintf(stderr, "USAGE: monty file\n");
+                exit(EXIT_FAILURE);
+        }
 
-	file = fopen(argv[1], "r");
-	bus.file = file;
-	if (!file)
-	{
-		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-		exit(EXIT_FAILURE);
-	}
+        file = fopen(argv[1], "r");
+        bus.file = file;
+        if (!file)
+        {
+                fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+                exit(EXIT_FAILURE);
+        }
 
-	while (read_line > 0)
-	{
-		char *content = NULL;
-		read_line = getline(&content, &size, file);
-		bus.content = content;
-		counter++;
+        while ((read_line = getline(&content, &size, file)) != -1)
+        {
+                counter++;
+                if (read_line > 0)
+                {
+                        execute(content, &stack, counter, file);
+                }
+        }
 
-		if (read_line > 0)
-		{
-			execute(content, &stack, counter, file);
-		}
+        free(content);
+        free_stack(stack);
+        fclose(file);
 
-		free(content);
-	}
-
-	free_stack(stack);
-	fclose(file);
-
-	return (0);
+        return 0;
 }
 
